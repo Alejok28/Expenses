25369f17241199cf380aa7de9af02cd9df678ad8
Alejo_k28 <alejo_k28@hotmail.com>
2017-12-17 18:08:23 -0500

    Add user authentication

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,12 @@
 class ExpensesController < ApplicationController
+  before_action :authenticate_user!
   def index
-     @expenses = Expense.order("date DESC")
+    @expenses = Expense.order("date DESC").concepto("%#{params[:concept]}%").id("%#{params[:category_id]}%")
+    @userexpenses=[]
+    @expenses.each do |expense|
+      if current_user == expense.user
+      @userexpenses.push(expense)
+      end
+    end
   end
 end
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,6 @@
 class Expense < ActiveRecord::Base
+  belongs_to :user
   belongs_to :category
+  scope :concepto, ->(c) { where("concept like ?", c) }
+  scope :id,       ->(id) { where("category_id like ?", id) }
 end
